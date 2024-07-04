040416dff0f1372b47061db382b13cb2dea61666
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-07-04 11:45:50 +0530

    Time: 7 ms (84.66%), Space: 14.2 MB (18.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
+        unordered_map<int, int> s;
 
-        unordered_map<int,int> s;
-
-        for(int i{0};i<nums.size();i++)
-        {
+        for (int i{0}; i < nums.size(); i++) {
             int x = target - nums[i];
-            if(s.find(x)!=s.end())
-            {
+            if (s.find(x) != s.end()) {
                 return {s[x], i};
             }
 
-            s[nums[i]]=i;
+            s[nums[i]] = i;
         }
 
         return {};
