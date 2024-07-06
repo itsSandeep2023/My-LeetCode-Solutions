3b9011e2bec76d72a479acda33faefe2faa0892e
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-07-06 13:16:21 +0530

    Time: 16 ms (81.54%), Space: 42.8 MB (58.88%) - LeetHub

---
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    string getDS(TreeNode* root, unordered_map<string, int> &mp, vector<TreeNode*> &ans)
+    {
+        if(!root) return "N";
+
+        string s = to_string(root->val) + ' ' + getDS(root->left, mp, ans) + ' ' + getDS(root->right, mp, ans);
+
+        if(mp[s]==1)
+            ans.push_back(root);
+        
+        mp[s]++;
+
+        return s;
+    }
+
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        unordered_map<string, int> mp;
+        vector<TreeNode*> ans;
+
+        getDS(root, mp, ans);
+
+        return ans;
+    }
+};
