bada55a6bdaf8aefb07f76c8deda7d8d274f5028
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-07-07 11:44:56 +0530

    Time: 2 ms (53.84%), Space: 7.2 MB (30.43%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int totalDrunk = numBottles; // Start by drinking all initial bottles
+        int totalDrunk = numBottles;
+        // Start by drinking all initial bottles
 
         while (numBottles >= numExchange) {
-            int newFullBottles =
-                numBottles / numExchange; // Full bottles after exchange
-            int remainingBottles =
-                numBottles %
-                numExchange; // Bottles left that cannot be exchanged
+            int newFullBottles = numBottles / numExchange;
+            // Full bottles after exchange
+            int remainingBottles = numBottles % numExchange;
+            // Bottles left that cannot be exchanged
 
-            totalDrunk +=
-                newFullBottles; // Add the new full bottles to the total drunk
-            numBottles =
-                newFullBottles + remainingBottles; // Update number of bottles
-                                                   // for the next iteration
+            totalDrunk += newFullBottles;
+            // Add the new full bottles to the total drunk
+            numBottles = newFullBottles + remainingBottles;
+            // Update number of bottles for the next iteration
         }
 
-        return totalDrunk; // Return the total number of bottles drunk
+        return totalDrunk;
+        // Return the total number of bottles drunk
     }
 };
