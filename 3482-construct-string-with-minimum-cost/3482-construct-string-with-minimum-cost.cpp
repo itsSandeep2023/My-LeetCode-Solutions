d09949bc40fffaeb9236c14507793429bb630826
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-07-10 00:53:41 +0530

    Time: 1200 ms (63.29%), Space: 286.8 MB (85.01%) - LeetHub

---
+++ 3482-construct-string-with-minimum-cost/3482-construct-string-with-minimum-cost.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    struct trie {
+        map<char, trie*> next;
+        int cost;
+        trie() { cost = INT_MAX; }
+    };
+
+    void insert(trie* root, string s, int cost) {
+        for (auto ch : s) {
+            if (!root->next[ch])
+                root->next[ch] = new trie();
+            root = root->next[ch];
+        }
+        // root->end = true;
+        root->cost = min(cost, root->cost);
+    }
+
+    int minimumCost(string target, vector<string>& words, vector<int>& costs) {
+        trie* root = new trie();
+        int n = costs.size();
+
+        vector<long long> dp(target.size() + 1, INT_MAX);
+        dp[target.size()] = 0;
+        for (int i = 0; i < n; i++) {
+            insert(root, words[i], costs[i]);
+        }
+        for (int i = target.size() - 1; i >= 0; i--) {
+            long long ans = INT_MAX;
+            trie* temp = root;
+            for (int j = i; j < target.size() && temp; j++) {
+
+                temp = temp->next[target[j]];
+                if (temp && temp->cost != INT_MAX) {
+                    ans = min(ans, temp->cost + dp[j + 1]);
+                }
+            }
+            dp[i] = ans;
+        }
+
+        return dp[0] >= INT_MAX ? -1 : dp[0];
+    }
+};
