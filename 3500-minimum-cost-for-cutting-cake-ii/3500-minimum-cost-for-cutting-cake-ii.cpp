e68da8f41e1b580bf3a6c1f467c361899cd477be
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-07-14 10:02:00 +0530

    Time: 489 ms (77.78%), Space: 234.8 MB (11.11%) - LeetHub

---
+++ 3500-minimum-cost-for-cutting-cake-ii/3500-minimum-cost-for-cutting-cake-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    long long minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
+        
+        sort(horizontalCut.rbegin(), horizontalCut.rend());
+        sort(verticalCut.rbegin(), verticalCut.rend());
+
+        long long i = 0;  
+        long long j = 0;  
+        long long hp = 1;
+        long long vp = 1;
+        long long ans = 0;
+
+        while (i < horizontalCut.size() || j < verticalCut.size()) {
+            if(i < horizontalCut.size() && j < verticalCut.size())
+            {
+                if (horizontalCut[i] >= verticalCut[j]) {
+                    ans += horizontalCut[i] * vp;
+                    hp++;
+                    i++;
+                } else {
+                    ans += verticalCut[j] * hp;
+                    vp++;
+                    j++;
+                }
+            }
+            else
+            {
+                if (i < horizontalCut.size()) {
+                    ans += horizontalCut[i] * vp;
+                    i++;
+                }
+                else if (j < verticalCut.size()) {
+                    ans += verticalCut[j] * hp;
+                    j++;
+                }
+            }
+        }
+
+
+        return ans;
+    }
+};
