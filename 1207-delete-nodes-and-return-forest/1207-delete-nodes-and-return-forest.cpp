ac226c8c5b1df8daa2d9b0bc485ca012d4830bc4
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-07-17 17:56:10 +0530

    Time: 18 ms (41.04%), Space: 27.2 MB (21.35%) - LeetHub

--- 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.cpp
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.cpp
@@ -30,7 +30,7 @@ public:
     }
 
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         
         unordered_set<int> st;
 
