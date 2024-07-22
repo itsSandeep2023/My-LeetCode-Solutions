da04ee3362a1146bd44f8d2858d9729310a2c0d5
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-07-22 19:22:37 +0530

    Time: 33 ms (40.77%), Space: 28.1 MB (23.78%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        priority_queue<pair<int, string>> hp;
+        for(int i{0};i<heights.size();i++)
+            hp.push({heights[i], names[i]});
+
+        vector<string> ans;
+
+        while(!hp.empty())
+        {
+            ans.push_back(hp.top().second);
+            hp.pop();
+        }
+
+        return ans;
+    }
+};
