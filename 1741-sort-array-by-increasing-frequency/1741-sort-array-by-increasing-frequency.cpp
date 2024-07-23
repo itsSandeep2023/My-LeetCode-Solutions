7a8801a3f8d377447132322484660180d6ba6780
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-07-23 12:21:26 +0530

    Time: 7 ms (55.39%), Space: 15.2 MB (8.78%) - LeetHub

---
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+        
+        sort(nums.begin(), nums.end(), [&freq](int a, int b) {
+            if (freq[a] == freq[b]) {
+                return a > b;
+            }
+            return freq[a] < freq[b];
+        });
+    
+        return nums;
+    }
+};
