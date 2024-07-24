afc238ebf8f7fb4615af07fc8787f499108230c1
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-07-24 11:48:43 +0530

    Time: 186 ms (75.76%), Space: 110.9 MB (29.87%) - LeetHub

---
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        ios::sync_with_stdio(false);
+
+        set<pair<int, int>> st;
+
+        for (int i{0}; i < nums.size(); i++) {
+            int t = nums[i];
+            int s{0};
+            int p{1};
+            do
+            {
+                s += (mapping[t % 10]) * p;
+                t /= 10;
+                p *= 10;
+            } while (t>0);
+
+            st.insert({s, i});
+        }
+
+        vector<int> ans;
+
+        for (auto& x : st) {
+            ans.push_back(nums[x.second]);
+        }
+
+        return ans;
+    }
+};
