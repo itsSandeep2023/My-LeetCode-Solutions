b99d575650aa89d918d5f1752342888dc373512a
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-07-26 17:35:04 +0530

    Time: 46 ms (14.51%), Space: 20.5 MB (8.62%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,51 +1,42 @@
 class Solution {
 public:
-    #define P pair<int, int>
-    #define init ios::sync_with_stdio(false);
+#define P pair<int, int>
 
-    void dijkstra(int n,  unordered_map<int, vector<P>> & adj, vector<int>& result, int s)
-    {
+    void dijkstra(int n, unordered_map<int, vector<P>>& adj,
+                  vector<int>& result, int s) {
         priority_queue<P, vector<P>, greater<P>> q;
         q.push({0, s});
 
-        while(!q.empty())
-        {
+        while (!q.empty()) {
             int d = q.top().first;
             int node = q.top().second;
             q.pop();
 
-            for(auto& p: adj[node])
-            {
+            for (auto& p : adj[node]) {
                 int nbr = p.first;
                 int dist = p.second;
 
-                if(d + dist < result[nbr])
-                {
-                    result[nbr] = d+dist;
-                    q.push({d+dist, nbr});
+                if (d + dist < result[nbr]) {
+                    result[nbr] = d + dist;
+                    q.push({d + dist, nbr});
                 }
-            } 
+            }
         }
     }
 
-    int findResultCity(int n,  vector<vector<int>>& SPM, int d)
-    {
+    int findResultCity(int n, vector<vector<int>>& SPM, int d) {
         int result = -1;
-        int smallest = 1e9+7;
+        int smallest = 1e9 + 7;
 
-        for(int i{0};i<n;i++)
-        {
+        for (int i{0}; i < n; i++) {
             int curr{0};
-            for(int j{0};j<n;j++)
-            {
-                if(i!=j and SPM[i][j] <= d)
-                {
+            for (int j{0}; j < n; j++) {
+                if (i != j and SPM[i][j] <= d) {
                     curr++;
                 }
             }
 
-            if(curr<=smallest)
-            {
+            if (curr <= smallest) {
                 smallest = curr;
                 result = i;
             }
@@ -55,17 +46,16 @@ public:
     }
 
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        init;
-        vector<vector<int>> SPM(n, vector<int>(n, 1e9+7));
+#define init ios::sync_with_stdio(false);
 
-        for(int i {0};i<n;i++)
-        {
+        vector<vector<int>> SPM(n, vector<int>(n, 1e9 + 7));
+
+        for (int i{0}; i < n; i++) {
             SPM[i][i] = 0;
         }
 
         unordered_map<int, vector<P>> adj;
-        for(auto &edge : edges)
-        {
+        for (auto& edge : edges) {
             int u = edge[0];
             int v = edge[1];
             int wt = edge[2];
@@ -74,8 +64,7 @@ public:
             adj[v].push_back({u, wt});
         }
 
-        for(int i{0};i<n;i++)
-        {
+        for (int i{0}; i < n; i++) {
             dijkstra(n, adj, SPM[i], i);
         }
 
