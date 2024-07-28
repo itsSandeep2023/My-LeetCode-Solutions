b445e13a2c42401ea154fa68a55f3474cfc85536
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-07-28 19:47:21 +0530

    Time: 475 ms (61.79%), Space: 199.4 MB (56.58%) - LeetHub

---
+++ 2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+#define P pair<int, int>
+
+    int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
+        ios::sync_with_stdio(false);
+
+        unordered_map<int, vector<int>> adj;
+
+        for (auto& e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
+        vector<int> d1(n + 1, INT_MAX);
+        vector<int> d2(n + 1, INT_MAX);
+        priority_queue<P, vector<P>, greater<P>> q;
+        q.push({0, 1});
+        d1[1] = 0;
+
+        while (!q.empty()) {
+            auto [timePassed, node] = q.top();
+            q.pop();
+
+            if (node == n and d2[node] != INT_MAX)
+                return d2[n];
+
+            int div = timePassed / change;
+            if (div % 2 == 1)
+                timePassed = change * (div + 1);
+
+            for (auto& nbr : adj[node]) {
+                if (d1[nbr] > timePassed + time) {
+                    d2[nbr] = d1[nbr];
+                    d1[nbr] = timePassed + time;
+                    q.push({timePassed + time, nbr});
+                } else if (d2[nbr] > timePassed + time and d1[nbr] != timePassed + time) {
+                    d2[nbr] = timePassed + time;
+                    q.push({timePassed + time, nbr});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
