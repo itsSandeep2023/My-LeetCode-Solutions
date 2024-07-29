e94159f493f60cfef7517eb0d456b268477b9d52
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-07-29 13:31:17 +0530

    Time: 30 ms (70.71%), Space: 11.5 MB (59.43%) - LeetHub

---
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int n = rating.size();
+
+        int ans{0};
+
+        for(int i{1};i<n-1;i++)
+        {
+            int ls{0}, rs{0},lm{0},rm{0};
+
+            for(int j{0};j<i;j++)
+            {
+                if(rating[j]<rating[i])
+                    ls++;
+                else if(rating[j]>rating[i])
+                    lm++;
+            }
+
+            for(int j{n-1};j>i;j--)
+            {
+                if(rating[j]<rating[i])
+                    rs++;
+                else if(rating[j]>rating[i])
+                    rm++;
+            }
+
+            ans+=ls*rm + lm*rs;
+        }
+
+        return ans;
+    }
+};
