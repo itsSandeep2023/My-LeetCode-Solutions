52fd613eae50c87e50b234a7990c6c895a529219
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-08-04 10:10:22 +0530

    Time: 131 ms (9.66%), Space: 27.2 MB (39.1%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        
         priority_queue<int> sums;
-        int mod = 1000000000 + 7;
+        int mod = 1e9 + 7;
 
         for (int i{0}; i < n; i++) {
             int csum{0};
