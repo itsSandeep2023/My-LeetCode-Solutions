99ae49f5bf70d02c170f0194e96fd3ac4d131898
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-08-08 10:49:43 +0530

    Time: 7 ms (86.9%), Space: 14.1 MB (8.31%) - LeetHub

--- 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart,
                                         int cStart) {
+        ios::sync_with_stdio(false);
+                                            
         vector<vector<int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
         vector<vector<int>> ans;
@@ -12,8 +14,7 @@ public:
         ans.push_back({rStart, cStart});
 
         while (ans.size() < rows * cols) {
-            if (dir == 0 or dir == 2)
-            {
+            if (dir == 0 or dir == 2) {
                 steps++;
             }
 
