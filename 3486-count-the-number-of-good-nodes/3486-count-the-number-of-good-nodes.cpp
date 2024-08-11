66158a99f2a9723ffe8739d45593ef21f64f3cdc
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-08-12 01:27:53 +0530

    Time: 1314 ms (100%), Space: 456.4 MB (50%) - LeetHub

---
+++ 3486-count-the-number-of-good-nodes/3486-count-the-number-of-good-nodes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int dfs(int node, int parent, vector<vector<int>>& tree, vector<int>& sts,
+            int& goodNodes) {
+        int st = 1;
+        unordered_set<int> css;
+
+        for (int neighbor : tree[node]) {
+            if (neighbor == parent)
+                continue;
+            int child_size = dfs(neighbor, node, tree, sts, goodNodes);
+            css.insert(child_size);
+            st += child_size;
+        }
+
+        sts[node] = st;
+
+        if (css.size() <= 1) {
+            goodNodes++;
+        }
+
+        return st;
+    }
+
+    int countGoodNodes(vector<vector<int>>& edges) {
+        int n = edges.size() + 1;
+        vector<vector<int>> tree(n);
+        vector<int> sts(n, 0);
+        int goodNodes = 0;
+
+        for (auto& edge : edges) {
+            tree[edge[0]].push_back(edge[1]);
+            tree[edge[1]].push_back(edge[0]);
+        }
+
+        dfs(0, -1, tree, sts, goodNodes);
+
+        return goodNodes;
+    }
+};
