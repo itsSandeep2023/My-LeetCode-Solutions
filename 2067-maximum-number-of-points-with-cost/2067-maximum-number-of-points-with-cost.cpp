46b9f6286fb8c44b33c9ab38dbf42e61d54d33c8
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-08-18 00:12:07 +0530

    Time: 130 ms (99.18%), Space: 116 MB (49.86%) - LeetHub

--- 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.cpp
+++ 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.cpp
@@ -3,6 +3,7 @@ public:
 #define ll long long
 
     long long maxPoints(vector<vector<int>>& points) {
+        ios::sync_with_stdio(false);
 
         int m = points.size();
         int n = points[0].size();
@@ -25,7 +26,7 @@ public:
             for(int i{1};i<n;i++)
             {
                 left[i] = max(left[i-1]-1, prev[i]);
-                right[n-1-i] = max(right[n-i]-1, prev[n-i-1]);
+                right[n-1-i] = max(right[n-i]-1, prev[n-1-i]);
             }
             
             for(int i{0}; i<n; i++)
