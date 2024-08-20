8f18b4eabbcbc18cb59c2bb8cc12b264b9f8c295
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-08-21 02:24:09 +0530

    Time: 17 ms (61.66%), Space: 10.4 MB (80.57%) - LeetHub

--- 1240-stone-game-ii/1240-stone-game-ii.cpp
+++ 1240-stone-game-ii/1240-stone-game-ii.cpp
@@ -3,42 +3,34 @@ public:
     int n;
     int dp[2][101][101];
 
-    int go(vector<int>& piles, int person, int i, int m)
-    {
-        if(i>=n) return 0;
+    int go(vector<int>& piles, int person, int i, int m) {
+        if (i >= n)
+            return 0;
 
-        if(dp[person][i][m]!=-1)
+        if (dp[person][i][m] != -1)
             return dp[person][i][m];
 
         int ans = person == 1 ? -1 : INT_MAX;
 
-
         int stones{0};
 
-        for(int j{1}; j<= min(2*m, n-i); j++)
-        {
-            stones += piles[i+j-1];
+        for (int j{1}; j <= min(2 * m, n - i); j++) {
+            stones += piles[i + j - 1];
 
-            if(person==1)
-            {
-                ans = max(ans, stones + go(piles, 0, i+j, max(m, j)));
-            }
-            else
-            {
-                ans = min(ans, go(piles, 1, i+j, max(m, j)));
+            if (person == 1) {
+                ans = max(ans, stones + go(piles, 0, i + j, max(m, j)));
+            } else {
+                ans = min(ans, go(piles, 1, i + j, max(m, j)));
             }
         }
 
         return dp[person][i][m] = ans;
     }
 
-
     int stoneGameII(vector<int>& piles) {
-        ios::sync_with_stdio(false);
-
         n = piles.size();
         memset(dp, -1, sizeof(dp));
 
-        return go(piles, 1, 0, 1);    
+        return go(piles, 1, 0, 1);
     }
 };
