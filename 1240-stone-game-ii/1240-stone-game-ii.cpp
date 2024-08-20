363d8528798081bfdccb7fb13c386775c4fe202b
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-08-21 02:23:07 +0530

    Time: 10 ms (82.6%), Space: 10.4 MB (88.69%) - LeetHub

---
+++ 1240-stone-game-ii/1240-stone-game-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int n;
+    int dp[2][101][101];
+
+    int go(vector<int>& piles, int person, int i, int m)
+    {
+        if(i>=n) return 0;
+
+        if(dp[person][i][m]!=-1)
+            return dp[person][i][m];
+
+        int ans = person == 1 ? -1 : INT_MAX;
+
+
+        int stones{0};
+
+        for(int j{1}; j<= min(2*m, n-i); j++)
+        {
+            stones += piles[i+j-1];
+
+            if(person==1)
+            {
+                ans = max(ans, stones + go(piles, 0, i+j, max(m, j)));
+            }
+            else
+            {
+                ans = min(ans, go(piles, 1, i+j, max(m, j)));
+            }
+        }
+
+        return dp[person][i][m] = ans;
+    }
+
+
+    int stoneGameII(vector<int>& piles) {
+        n = piles.size();
+        memset(dp, -1, sizeof(dp));
+
+        return go(piles, 1, 0, 1);    
+    }
+};
