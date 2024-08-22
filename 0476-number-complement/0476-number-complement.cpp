b8cd2c2b94bf107a65c24fea7a83e988000e62e5
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-08-22 12:23:16 +0530

    Time: 0 ms (100%), Space: 7.7 MB (8.34%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int findComplement(int num) {
-        return num ^ ((1U << ((int)(log2(num)) + 1)) - 1);
+        int ans{0};
+        int mask{1};
+
+        if ((num & mask) == 0)
+            ans ^= mask;
+
+        for (int i{0}; i < (int)(log2(num)); i++) {
+            mask <<= 1;
+            if ((num & mask) == 0)
+                ans ^= mask;
+            cout << mask << " " << ans << endl;
+        }
+
+        return ans;
     }
 };
