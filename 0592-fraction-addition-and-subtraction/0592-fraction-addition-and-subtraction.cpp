bd7e6a5a6cf5e11798f1855d3d03ea1145ecefb8
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-08-24 00:39:56 +0530

    Time: 3 ms (50.39%), Space: 7.7 MB (70.54%) - LeetHub

---
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string fractionAddition(string expression) {
+        int nume = 0;
+        int deno = 1;
+
+        int i{0};
+        int n = expression.size();
+        while (i < n) {
+            int curNume{0};
+            int curDeno{0};
+
+            bool isNeg = (expression[i] == '-');
+
+            if (expression[i] == '-' or expression[i] == '+')
+                i++;
+
+            while (i < n and isdigit(expression[i])) {
+                curNume = curNume * 10 + (expression[i] - '0');
+                i++;
+            }
+
+            if (isNeg)
+                curNume *= -1;
+
+            i++;
+
+            while (i < n and isdigit(expression[i])) {
+                curDeno = curDeno * 10 + (expression[i] - '0');
+                i++;
+            }
+
+            nume = nume * curDeno + curNume * deno;
+            deno = deno * curDeno;
+        }
+
+        int GCD = abs(__gcd(nume, deno));
+        nume /= GCD;
+        deno /= GCD;
+
+        return to_string(nume) + '/' + to_string(deno);
+    }
+};
