a609891050832b0e3ffcc844834b23c097f1a05f
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-08-24 23:19:36 +0530

    Time: 1173 ms (63.69%), Space: 448.4 MB (31.91%) - LeetHub

--- 3486-count-the-number-of-good-nodes/3486-count-the-number-of-good-nodes.cpp
+++ 3486-count-the-number-of-good-nodes/3486-count-the-number-of-good-nodes.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    int dfs(int node, int parent, vector<vector<int>>& tree, vector<int>& sts,
-            int& goodNodes) {
+    int dfs(int node, int parent, vector<vector<int>>& tree, int& goodNodes) {
         int st = 1;
         unordered_set<int> css;
 
         for (int neighbor : tree[node]) {
             if (neighbor == parent)
                 continue;
-            int child_size = dfs(neighbor, node, tree, sts, goodNodes);
+            int child_size = dfs(neighbor, node, tree, goodNodes);
             css.insert(child_size);
             st += child_size;
         }
-
-        sts[node] = st;
 
         if (css.size() <= 1) {
             goodNodes++;
@@ -25,10 +22,9 @@ public:
     int countGoodNodes(vector<vector<int>>& edges) {
         ios::sync_with_stdio(false);
         cin.tie(0);
-        
+
         int n = edges.size() + 1;
         vector<vector<int>> tree(n);
-        vector<int> sts(n, 0);
         int goodNodes = 0;
 
         for (auto& edge : edges) {
@@ -36,7 +32,7 @@ public:
             tree[edge[1]].push_back(edge[0]);
         }
 
-        dfs(0, -1, tree, sts, goodNodes);
+        dfs(0, -1, tree, goodNodes);
 
         return goodNodes;
     }
