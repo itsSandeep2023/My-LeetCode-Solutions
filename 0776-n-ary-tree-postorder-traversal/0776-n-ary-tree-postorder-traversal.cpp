019996271ebc9c23582af681ac28983811d20dfd
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-08-26 14:49:05 +0530

    Time: 13 ms (64.15%), Space: 15.3 MB (21.73%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    void pot(Node* r, vector<int>& ans)
+    {
+        if(!r) return;
+
+        for(auto *child : r->children)
+            pot(child, ans);
+
+        ans.push_back(r->val);
+    }
+
+    vector<int> postorder(Node* root) {
+        vector<int> ans;
+        pot(root, ans);
+        return ans;
+    }
+};
