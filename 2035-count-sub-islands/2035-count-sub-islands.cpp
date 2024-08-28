77313524c85f4928e284e6c1dd7ce08e60d1c609
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-08-28 23:04:27 +0530

    Time: 164 ms (98.86%), Space: 94.8 MB (69.62%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.cpp
+++ 2035-count-sub-islands/2035-count-sub-islands.cpp
@@ -19,7 +19,6 @@ public:
 
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         ios::sync_with_stdio(false);
-        cin.tie(nullptr);
         
         int m = grid1.size();
         int n = grid1[0].size();
