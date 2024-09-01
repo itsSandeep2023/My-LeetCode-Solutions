3074453e7904f7587f499e03e278d842d779c118
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-09-01 12:10:56 +0530

    Time: 79 ms (69.46%), Space: 88.1 MB (73.42%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         ios::sync_with_stdio(false);
-        
+
         if (original.size() != m * n)
             return {};
 
         vector<vector<int>> ans(m, vector<int>(n));
 
         for (int i{0}; i < original.size(); i++) {
-                ans[i/n][i%n] = original[i];
+            ans[i / n][i % n] = original[i];
         }
 
         return ans;
