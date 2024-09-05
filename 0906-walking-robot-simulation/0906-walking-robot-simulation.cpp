40e403d576244a82a5be14055a8f530d45776348
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-09-05 12:20:34 +0530

    Time: 100 ms (37.5%), Space: 47.8 MB (30.16%) - LeetHub

--- 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
@@ -1,43 +1,69 @@
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        ios::sync_with_stdio(false);
-        
-        unordered_set<string> st;
+        int dir1[] = {1, 0, -1, 0};
+        int dir2[] = {0, 1, 0, -1};
 
-        for (auto& obs : obstacles) {
-            string key = to_string(obs[0]) + "_" + to_string(obs[1]);
-            st.insert(key);
+        unordered_map<int, vector<int>> xmp;
+        unordered_map<int, vector<int>> ymp;
+
+        for (const auto& vec : obstacles) {
+            xmp[vec[1]].push_back(vec[0]);
+            ymp[vec[0]].push_back(vec[1]);
         }
 
         int x{0}, y{0};
 
+        int cd = 0;
+
         int ans{0};
 
-        pair<int, int> dir = {0, 1};
-
         for (const auto& com : commands) {
-            if (com == -2) {
-                dir = {-dir.second, dir.first};
-            } else if (com == -1) {
-                dir = {dir.second, -dir.first};
-            } else {
-                for (int i{0}; i < com; i++) {
-                    int newX = x + dir.first;
-                    int newY = y + dir.second;
+            if (com == -1)
+                cd = (cd + 1) % 4;
+            else if (com == -2)
+                cd -= (cd == 0) ? -3 : 1;
+            else {
+                int d, t;
 
-                    string nextKey = to_string(newX) + "_" + to_string(newY);
-
-                    if (st.count(nextKey)) {
-                        break;
-                    }
-
-                    x = newX;
-                    y = newY;
+                bool neg = false;
+                if (cd == 0) {
+                    d = x;
+                    t = y;
+                } else if (cd == 1) {
+                    d = y;
+                    t = x;
+                } else if (cd == 2) {
+                    d = x;
+                    t = y;
+                    neg = true;
+                } else {
+                    d = y;
+                    t = x;
+                    neg = true;
                 }
 
-                ans = max(ans, (x * x + y * y));
+                vector<int>& cmp = (cd == 1 or cd == 3) ? xmp[d] : ymp[d];
+                int dif{com};
+                for (auto& it : cmp) {
+                    if (neg) {
+                        if (it < t) {
+                            dif = min(dif, abs(t - it) - 1);
+                        }
+                    } else if (it > t) {
+                        dif = min(dif, (it - t - 1));
+                    }
+                }
+
+                cout << dif << " " << neg << "  ";
+                if (cd == 1 or cd == 3) {
+                    x += neg ? -(dif) : (dif);
+                } else {
+                    y += neg ? -dif : dif;
+                }
             }
+
+            ans = max(ans, x * x + y * y);
         }
 
         return ans;
