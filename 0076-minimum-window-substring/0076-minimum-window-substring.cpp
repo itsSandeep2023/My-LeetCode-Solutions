6cbaa936064f4b54bc03c422f3324a7c66515306
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-09-08 02:29:24 +0530

    Time: 11 ms (76.75%), Space: 10.7 MB (67.53%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int FP[256] = {0};
+        int FS[256] = {0};
+
+        for(const auto& ch : t)
+            FP[ch]++;
+
+        int cnt{0};
+        int start{0};
+        int start_idx{-1};
+        int minSoFar = INT_MAX;
+        int window_size {0};
+
+        for(int i{0};i<s.size();i++)
+        {
+            char ch = s[i];
+            FS[ch]++;
+
+            if(FP[ch]!=0 and FS[ch] <= FP[ch])
+            {
+                cnt++;
+            }
+
+            if(cnt == t.size())
+            {
+                while(FP[s[start]]==0 or FS[s[start]] > FP[s[start]])
+                {
+                    FS[s[start]]--;
+                    start++;
+                }
+
+                int window_size =  i - start + 1;
+
+                if(window_size < minSoFar)
+                {
+                    minSoFar = window_size;
+                    start_idx = start;
+                }
+            }
+        }
+
+        if(start_idx == -1) return "";
+
+        return s.substr(start_idx, minSoFar);
+    }
+};
