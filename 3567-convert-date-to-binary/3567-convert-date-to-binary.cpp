98f1754669878a0290cff315c3ceb4c2d5fe9c07
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-09-08 10:36:33 +0530

    Time: 0 ms (100%), Space: 8.8 MB (37.5%) - LeetHub

--- 3567-convert-date-to-binary/3567-convert-date-to-binary.cpp
+++ 3567-convert-date-to-binary/3567-convert-date-to-binary.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     string convertDateToBinary(string date) {
-        int year = stoi(date.substr(0, 4));
-        int mon = stoi(date.substr(5, 2));
-        int dt = stoi(date.substr(8, 2));
-
-        string ans;
-
-        while (dt > 0) {
-            ans = (dt & 1 ? "1" : "0") + ans;
-            dt /= 2;
-        }
-        ans = "-" + ans;
-        while (mon > 0) {
-            ans = (mon & 1 ? "1" : "0") + ans;
-            mon /= 2;
-        }
-        ans = "-" + ans;
-        while (year > 0) {
-            ans = (year & 1 ? "1" : "0") + ans;
-            year/=2;
-        }
-        return ans;
+        auto gao = [&](int x) {
+            string ret;
+            for (; x; x >>= 1)
+                ret.push_back((x & 1) + '0');
+            reverse(ret.begin(), ret.end());
+            return ret;
+        };
+        return gao(stoi(date.substr(0, 4))) + "-" +
+               gao(stoi(date.substr(5, 2))) + "-" +
+               gao(stoi(date.substr(8, 2)));
     }
 };
