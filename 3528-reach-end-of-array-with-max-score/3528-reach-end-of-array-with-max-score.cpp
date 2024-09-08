644143da5153ff291b02b83a6e31f5b046d66f70
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-09-08 10:32:14 +0530

    Time: 185 ms (100%), Space: 171.5 MB (30%) - LeetHub

--- 3528-reach-end-of-array-with-max-score/3528-reach-end-of-array-with-max-score.cpp
+++ 3528-reach-end-of-array-with-max-score/3528-reach-end-of-array-with-max-score.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     long long findMaximumScore(vector<int>& nums) {
-        int n = nums.size();
-        long long res = 0;
-        int x = nums[0], j = 1, i = 0;
-
-        while (j < n - 1) {
-            if (nums[i] < nums[j]) {
-                res += x * 1LL * (j - i);
-                i = j;
-                x = nums[i];
-            }
-            j++;
+        long long ans = 0;
+        int mx = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            ans += mx;
+            mx = max(mx, nums[i]);
         }
-
-        return res + x * 1LL * (n - 1 - i);
+        return ans;
     }
 };
