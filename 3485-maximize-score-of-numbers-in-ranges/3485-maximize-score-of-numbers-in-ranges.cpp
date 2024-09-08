94a6c2047f3504486967abc471dceda050decef6
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-09-08 10:49:57 +0530

    Time: 233 ms (100%), Space: 109.6 MB (100%) - LeetHub

--- 3485-maximize-score-of-numbers-in-ranges/3485-maximize-score-of-numbers-in-ranges.cpp
+++ 3485-maximize-score-of-numbers-in-ranges/3485-maximize-score-of-numbers-in-ranges.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int maxPossibleScore(vector<int>& start, int d) {
-        ios::sync_with_stdio(false);
-        
         sort(start.begin(), start.end());
 
         auto check = [&](long long lim) {
             long long now = start[0];
             for (int i = 1; i < start.size(); i++) {
                 now = max(now + lim, 1LL * start[i]);
-                if (now > start[i] + d) return false;
+                if (now > start[i] + d)
+                    return false;
             }
             return true;
         };
@@ -17,8 +16,10 @@ public:
         long long head = 0, tail = 2e9;
         while (head < tail) {
             long long mid = (head + tail + 1) >> 1;
-            if (check(mid)) head = mid;
-            else tail = mid - 1;
+            if (check(mid))
+                head = mid;
+            else
+                tail = mid - 1;
         }
         return head;
     }
