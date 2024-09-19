8f8b3bf98d25b019cdd94751f0d8584bd492fd2b
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-09-20 00:58:42 +0530

    Time: 174 ms (63.83%), Space: 176.2 MB (56.63%) - LeetHub

--- 3518-maximum-multiplication-score/3518-maximum-multiplication-score.cpp
+++ 3518-maximum-multiplication-score/3518-maximum-multiplication-score.cpp
@@ -1,7 +1,5 @@
 class Solution {
 public:
-    #define ll long long
-
     long long maxScore(vector<int>& a, vector<int>& b) {
         int n = b.size();
 
@@ -12,22 +10,22 @@ public:
 
         for (int i = n - 1; i >= 3; i--) {
             long long x = (long long)(b[i]) * a[3];
-            dp3[i] = max(1ll* dp3[i + 1], x);
+            dp3[i] = max(dp3[i + 1], x);
         }
 
         for (int i = n - 2; i >= 2; i--) {
             long long x = (long long)(b[i]) * a[2];
-            dp2[i] = max(1ll*dp2[i + 1], x + 1ll*dp3[i + 1]);
+            dp2[i] = max(dp2[i + 1], x + dp3[i + 1]);
         }
 
         for (int i = n - 3; i >= 1; i--) {
             long long x = (long long)(b[i]) * a[1];
-            dp1[i] = max(1ll*dp1[i + 1], x + 1ll*dp2[i + 1]);
+            dp1[i] = max(dp1[i + 1], x + dp2[i + 1]);
         }
 
         for (int i = n - 4; i >= 0; i--) {
             long long x = (long long)(b[i]) * a[0];
-            dp0[i] = max(1ll*dp0[i + 1], x + 1ll*dp1[i + 1]);
+            dp0[i] = max(dp0[i + 1], x + dp1[i + 1]);
         }
 
         return dp0[0];
