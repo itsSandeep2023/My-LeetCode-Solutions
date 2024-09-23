07e7a994e4eaebc50aabb3433772b23ad95e41aa
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-09-24 00:43:50 +0530

    Time: 135 ms (52.37%), Space: 86.9 MB (49.49%) - LeetHub

---
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        ios::sync_with_stdio(false);
+
+        int n = s.size();
+
+        unordered_set<string> st(dictionary.begin(), dictionary.end());
+        vector<int> dp(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = 1 + dp[i + 1];
+
+            for (int j = i; j < n; j++) {
+                string curr = s.substr(i, j - i + 1);
+                if (st.count(curr))
+                    dp[i] = min(dp[i], dp[j + 1]);
+            }
+        }
+
+        return dp[0];
+    }
+};
