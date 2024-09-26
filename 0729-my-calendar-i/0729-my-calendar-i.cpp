b29a638eacab947b5d7aa1c6253a7de97f334d35
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-09-26 20:21:59 +0530

    Time: 56 ms (98.92%), Space: 42.9 MB (36.63%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,24 +1,20 @@
 class MyCalendar {
 public:
     set<pair<int, int>> st;
-    MyCalendar() {
-        
-    }
-    
+    MyCalendar() { ios::sync_with_stdio(false); }
+
     bool book(int start, int end) {
         auto it = st.upper_bound({start, end});
 
-        if(it != st.end() && end > it->second) {
+        if (it != st.end() && end > it->second) {
 
             return false;
-            
         }
-        
+
         st.insert({end, start});
         return true;
     }
 };
-
 
 /**
  * Your MyCalendar object will be instantiated and called as such:
