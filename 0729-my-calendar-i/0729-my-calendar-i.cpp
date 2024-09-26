467398564fffa0bec400c44ebe705bafc5966a1a
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-09-26 20:21:17 +0530

    Time: 77 ms (66.21%), Space: 42.8 MB (59.1%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -6,26 +6,19 @@ public:
     }
     
     bool book(int start, int end) {
-        //Find the first event that starts after or st the same time of {start, end}
-        auto it = st.lower_bound({start, end}); //Log(N)
+        auto it = st.upper_bound({start, end});
 
-        //Check if the current event overlaps with the next event
-        if(it != st.end() && it->first < end) {
+        if(it != st.end() && end > it->second) {
+
             return false;
+            
         }
-
-        //Check if the current event overlaps with the previous event
-        if(it != st.begin()) {
-            auto prevIt = prev(it);
-            if(start < prevIt->second) {
-                return false;
-            }
-        }
-
-        st.insert({start, end});
+        
+        st.insert({end, start});
         return true;
     }
 };
+
 
 /**
  * Your MyCalendar object will be instantiated and called as such:
