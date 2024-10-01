27d19ea80139c27702bea2ec6e770a6265e6e22c
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-10-01 14:59:49 +0530

    Time: 23 ms (86.56%), Space: 26.1 MB (29.97%) - LeetHub

---
+++ 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.cpp
@@ -0,0 +1,42 @@
+class CustomStack {
+public:
+    vector<int> st;
+    vector<int> inc;
+    int n;
+
+    CustomStack(int maxSize) {
+        n = maxSize;
+    }
+    
+    void push(int x) {
+        if(st.size() < n) {
+            st.push_back(x);
+            inc.push_back(0);
+        }
+    }
+    
+    int pop() {
+        if(st.size() == 0) {
+            return -1;
+        }
+
+        int idx = st.size()-1;
+        
+        if(idx > 0) {
+            inc[idx-1] += inc[idx];
+        }
+
+        int top_val = st[idx] + inc[idx];
+        st.pop_back();
+        inc.pop_back();
+        
+        return top_val;
+    }
+    
+    void increment(int k, int val) {
+        int idx = min(k, (int)st.size()) - 1;
+        if (idx >= 0) {
+            inc[idx] += val;
+        }
+    }
+};
