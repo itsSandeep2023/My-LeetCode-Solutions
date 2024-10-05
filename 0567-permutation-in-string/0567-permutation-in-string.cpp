c4f1980117893398ad0baa273628d0a9843b8196
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-10-05 21:32:57 +0530

    Time: 0 ms (100%), Space: 8.9 MB (50.93%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        ios::sync_with_stdio(false);
+        
         int n = s1.size();
         int m = s2.size();
 
-        if(n>m) return false;
-        
+        if (n > m)
+            return false;
+
         vector<int> freq(26, 0);
 
-        for(const auto& x : s1)
-            freq[x-'a']++;
+        for (const auto& x : s1)
+            freq[x - 'a']++;
 
-        vector<int> cur(26,0);
+        vector<int> cur(26, 0);
 
-        for(int i{0}; i<n-1; i++)
-        {
-            cur[s2[i]-'a']++;
+        for (int i{0}; i < n - 1; i++) {
+            cur[s2[i] - 'a']++;
         }
 
-        for(int i{0}; i<=(m-n); i++)
-        {
-            cur[s2[i+n-1] - 'a']++;
+        for (int i{0}; i <= (m - n); i++) {
+            cur[s2[i + n - 1] - 'a']++;
             bool yes = true;
-            for(int j{0}; j<26; j++)
-            {
-                if(freq[j] != cur[j])
-                {
+            for (int j{0}; j < 26; j++) {
+                if (freq[j] != cur[j]) {
                     yes = false;
                     break;
                 }
             }
 
-            if(yes) return true;
+            if (yes)
+                return true;
 
-            cur[s2[i]-'a']--;
-        }        
+            cur[s2[i] - 'a']--;
+        }
 
         return false;
     }
