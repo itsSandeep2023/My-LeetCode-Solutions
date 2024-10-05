b276198d9bcfaad0d503673913400c6671d3b302
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-10-05 21:32:36 +0530

    Time: 4 ms (81.5%), Space: 8.9 MB (42.93%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+
+        if(n>m) return false;
+        
+        vector<int> freq(26, 0);
+
+        for(const auto& x : s1)
+            freq[x-'a']++;
+
+        vector<int> cur(26,0);
+
+        for(int i{0}; i<n-1; i++)
+        {
+            cur[s2[i]-'a']++;
+        }
+
+        for(int i{0}; i<=(m-n); i++)
+        {
+            cur[s2[i+n-1] - 'a']++;
+            bool yes = true;
+            for(int j{0}; j<26; j++)
+            {
+                if(freq[j] != cur[j])
+                {
+                    yes = false;
+                    break;
+                }
+            }
+
+            if(yes) return true;
+
+            cur[s2[i]-'a']--;
+        }        
+
+        return false;
+    }
+};
