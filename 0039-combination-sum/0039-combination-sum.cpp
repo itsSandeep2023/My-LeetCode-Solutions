e6bd54ef5e3035982501395760281d7bcfb67164
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-10-06 17:15:47 +0530

    Time: 0 ms (100%), Space: 12.5 MB (92.55%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void go(vector<int>& cnd, int& target, int idx, int cursum, vector<vector<int>>& res, vector<int> &vec)
+    {
+        if(cursum == target)
+        {
+            res.push_back(vec);
+            return;
+        }
+
+        if(idx>=cnd.size() or cursum>target) return;
+
+        
+        vec.push_back(cnd[idx]);
+        go(cnd, target, idx, cursum + cnd[idx], res, vec);
+        vec.pop_back();
+        go(cnd, target, idx + 1, cursum, res, vec);
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& cnd, int target) {
+        vector<vector<int>> res;
+        vector<int> vec;
+        int cursum{0};
+
+        go(cnd, target, 0, cursum, res, vec);
+
+        return res;
+    }
+};
