36eb1974a33772e408478f67e9599b67a55c703b
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-10-07 21:15:04 +0530

    Time: 885 ms (6.63%), Space: 81.7 MB (6.07%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -1,5 +1,5 @@
 class AllOne {
-private:
+public:
     struct Node {
         int count;
         list<string> keys;
@@ -11,51 +11,55 @@ private:
 
     Node *head, *tail;
 
-    Node* addNodeAfter(Node* prevNode, int count) {
-        Node* newNode = new Node(count);
-        newNode->next = prevNode->next;
-        newNode->prev = prevNode;
-        if (prevNode->next) {
-            prevNode->next->prev = newNode;
-        }
-        prevNode->next = newNode;
-        if (tail == prevNode) {
-            tail = newNode;
-        }
-        return newNode;
-    }
-
-    void removeNode(Node* node) {
-        node->prev->next = node->next;
-        if (node->next) {
-            node->next->prev = node->prev;
-        }
-        if (tail == node) {
-            tail = node->prev;
-        }
-        delete node;
-    }
-
-public:
     AllOne() {
         head = new Node(0);
         tail = head;
     }
 
+    void addNode(Node* prevNode, int count) {
+        Node* newNode = new Node(count);
+        newNode->next = prevNode->next;
+        newNode->prev = prevNode;
+        if (newNode->next != nullptr) {
+            newNode->next->prev = newNode;
+        }
+
+        prevNode->next = newNode;
+
+        if (prevNode == tail) {
+            tail = newNode;
+        }
+    }
+
+    void removeNode(Node* node) {
+        node->prev->next = node->next;
+        if (node->next != nullptr) {
+            node->next->prev = node->prev;
+        }
+
+        if (node == tail) {
+            tail = node->prev;
+        }
+
+        delete node;
+    }
+
     void inc(string key) {
-        if (mp.find(key) == mp.end()) {
-            if (head->next == nullptr || head->next->count != 1) {
-                addNodeAfter(head, 1);
+        if (!mp.count(key)) {
+            if (head->next == nullptr or head->next->count != 1) {
+                addNode(head, 1);
             }
+
             head->next->keys.push_front(key);
             mp[key] = head->next;
         } else {
             Node* curNode = mp[key];
             int curCount = curNode->count;
-            if (curNode->next == nullptr ||
+            if (curNode->next == nullptr or
                 curNode->next->count != curCount + 1) {
-                addNodeAfter(curNode, curCount + 1);
+                addNode(curNode, curCount + 1);
             }
+
             curNode->next->keys.push_front(key);
             mp[key] = curNode->next;
             curNode->keys.remove(key);
@@ -69,25 +73,43 @@ public:
         Node* curNode = mp[key];
         int curCount = curNode->count;
 
-        curNode->keys.remove(key);
         if (curCount == 1) {
             mp.erase(key);
-        } else {
-            if (curNode->prev == head || curNode->prev->count != curCount - 1) {
-                addNodeAfter(curNode->prev, curCount - 1);
+        } 
+        else {
+            if (curNode->prev->count != curCount - 1) {
+                addNode(curNode->prev, curCount - 1);
             }
+
             curNode->prev->keys.push_front(key);
             mp[key] = curNode->prev;
         }
-
+        curNode->keys.remove(key);
         if (curNode->keys.empty()) {
             removeNode(curNode);
         }
     }
 
-    string getMaxKey() { return (tail == head) ? "" : tail->keys.front(); }
+    string getMaxKey() {
+        if (tail == head)
+            return "";
+
+        return tail->keys.front();
+    }
 
     string getMinKey() {
-        return (head->next == nullptr) ? "" : head->next->keys.front();
+        if (!head->next)
+            return "";
+
+        return head->next->keys.front();
     }
 };
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne* obj = new AllOne();
+ * obj->inc(key);
+ * obj->dec(key);
+ * string param_3 = obj->getMaxKey();
+ * string param_4 = obj->getMinKey();
+ */
