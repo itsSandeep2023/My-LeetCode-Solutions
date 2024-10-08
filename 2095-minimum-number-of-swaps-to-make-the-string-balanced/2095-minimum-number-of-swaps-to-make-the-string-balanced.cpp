53a86d2af8a2986ff56587c727eb2ea5132d3f58
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-10-08 13:50:01 +0530

    Time: 108 ms (48.8%), Space: 32.7 MB (54.72%) - LeetHub

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        int n = s.size();
+
+        int i{0}, j = n-1;
+
+        int ans{0};
+
+        int l{0}, r{0};
+
+        while(i<j)
+        {
+           while(i<j and (s[i] == '[' or l>0))
+           {
+                if(s[i] == '[')
+                    l++;
+                else
+                    l--;
+                i++;
+           }
+
+           while(i<j and (s[j] == ']' or r>0))
+           {
+                if(s[j] == ']')
+                    r++;
+                else
+                    r--;
+                j--;
+           }
+
+           if(i>=j)
+                break;
+
+           l++;
+           r++;
+           i++;
+           j--;
+           ans++;
+        }
+
+        return ans;
+    }
+};
