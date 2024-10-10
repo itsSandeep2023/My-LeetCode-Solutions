c3f025a4d00e0965841f517c2426978c75edef07
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-10-10 13:11:21 +0530

    Time: 2854 ms (5.01%), Space: 45.4 MB (46.87%) - LeetHub

---
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        int ans{0};
+        int mx = INT_MAX;
+
+        int n = nums.size();
+
+        for(int i{0}; i<n-1; i++)
+        {
+            if(nums[i]>=mx)
+                continue;
+
+            mx = min(mx, nums[i]);
+            int j = n-1;
+
+            while(j>i)
+            {
+                if(nums[i] <= nums[j])
+                {
+                    ans = max(ans, (j-i));
+                    break;
+                }
+                j--;
+            }
+        }      
+
+        return ans;     
+    }
+};
