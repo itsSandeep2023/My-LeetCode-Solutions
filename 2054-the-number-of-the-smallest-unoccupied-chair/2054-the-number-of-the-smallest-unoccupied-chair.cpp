8742527ae6762f16691b6ede59eca0e129e48a43
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-10-11 10:49:58 +0530

    Time: 116 ms (99.31%), Space: 57 MB (90.03%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
         ios::sync_with_stdio(false);
+        cin.tie(0);
             
         auto frnd = times[targetFriend];
         sort(times.begin(), times.end());
