ba50c612ef71c5f2f85dc9d9b0ac4ee4ce8fad76
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-10-11 10:45:53 +0530

    Time: 145 ms (61.86%), Space: 57.1 MB (88.66%) - LeetHub

---
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        auto frnd = times[targetFriend];
+        sort(times.begin(), times.end());
+
+        priority_queue<int, vector<int>, greater<int>> emptySeats;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> takenSeats;
+
+        for (int i = 0; i < times.size(); ++i) emptySeats.push(i);
+
+        for (auto &vec : times) {
+            int arrival = vec[0], leave = vec[1];
+
+            while (!takenSeats.empty() && takenSeats.top().first <= arrival) {
+                emptySeats.push(takenSeats.top().second);
+                takenSeats.pop();
+            }
+
+            int seat = emptySeats.top();
+            emptySeats.pop();
+
+            if (arrival == frnd[0] and leave == frnd[1]) return seat;
+
+            takenSeats.push({leave, seat});
+        }
+
+        return -1;  
+    }
+};
