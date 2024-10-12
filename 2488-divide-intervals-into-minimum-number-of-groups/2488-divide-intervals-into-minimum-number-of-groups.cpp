40c14d7ee058237d45e8076c9c848d56dbaab160
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-10-12 12:04:49 +0530

    Time: 258 ms (83.78%), Space: 92.5 MB (97.75%) - LeetHub

---
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& itv) {
+        int n = itv.size();
+        if(n==1) return 1;
+        sort(itv.begin(), itv.end());
+
+        priority_queue<int, vector<int>, greater<int>> b;
+        b.push(itv[0][1]);
+
+        for(int i{1}; i<n; i++)
+        {
+            if(b.top() < itv[i][0])
+                b.pop();
+            b.push(itv[i][1]);
+        }
+
+        return b.size();
+    }
+};
