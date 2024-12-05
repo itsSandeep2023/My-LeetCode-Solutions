b333ed8f46110456929763eccc05cac9d6927a86
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-05 19:18:56 +0530

    Time: 26 ms (30.46%), Space: 21.8 MB (35.59%) - LeetHub

---
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int n = start.size();
+
+        int i{0}, j{0};
+
+        int scnt{0}, tcnt{0};
+
+        for (int i{0}; i < n; i++) {
+            if (start[i] == '_')
+                scnt++;
+            if (target[i] == '_')
+                tcnt++;
+        }
+
+        if (scnt != tcnt)
+            return false;
+
+        while (i < n) {
+            if (start[i] == '_')
+                i++;
+
+            if (target[j] == '_')
+                j++;
+
+            if (start[i] != '_' and target[j] != '_') {
+                if (start[i] != target[j])
+                    return false;
+
+                if (start[i] == 'L' and i < j)
+                    return false;
+
+                if (start[i] == 'R' and i > j)
+                    return false;
+
+                i++;
+                j++;
+            }
+        }
+
+        return true;
+        ;
+    }
+};
