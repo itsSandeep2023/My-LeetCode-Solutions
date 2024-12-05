c28043f21ca11ab7bf5150a28c3fd2bab4749a8e
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-05 20:11:39 +0530

    Time: 14 ms (45.4%), Space: 21.6 MB (38.74%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
     bool canChange(string start, string target) {
+        ios::sync_with_stdio(false);
+        
         int n = start.size();
 
         int i{0}, j{0};
 
-        while (i < n or j < n) {
-            while(i<n and start[i] == '_')
+        int scnt{0}, tcnt{0};
+
+        for (int i{0}; i < n; i++) {
+            if (start[i] == '_')
+                scnt++;
+            if (target[i] == '_')
+                tcnt++;
+        }
+
+        if (scnt != tcnt)
+            return false;
+
+        while (i < n) {
+            if (start[i] == '_')
                 i++;
 
-            while(j<n and target[j] == '_')
+            if (target[j] == '_')
                 j++;
 
-            if (i==n or j==n)
-                return (i==n and j==n);
+            if (start[i] != '_' and target[j] != '_') {
+                if (start[i] != target[j])
+                    return false;
 
-            if (start[i] != target[j])
-                return false;
+                if (start[i] == 'L' and i < j)
+                    return false;
 
-            if (start[i] == 'L' and i < j)
-                return false;
+                if (start[i] == 'R' and i > j)
+                    return false;
 
-            if (start[i] == 'R' and i > j)
-                return false;
-
-            i++;
-            j++;
+                i++;
+                j++;
+            }
         }
 
         return true;
