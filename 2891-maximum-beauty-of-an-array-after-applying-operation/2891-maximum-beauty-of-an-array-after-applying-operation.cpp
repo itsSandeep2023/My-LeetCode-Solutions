d70ff18645f33c2b060a1a88514eab3eff4bdd28
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-11 18:17:48 +0530

    Time: 85 ms (45.32%), Space: 128.4 MB (35.14%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
+        ios::sync_with_stdio(false);
+        
         int n = nums.size();
         vector<pair<int, int>> t;
 
