627954a0a610e7bed7b6d9f2a69a3ffae55797e3
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-11 18:42:34 +0530

    Time: 43 ms (84.29%), Space: 104.9 MB (84.24%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<pair<int, int>> t;
+        sort(nums.begin(), nums.end());
 
-        for(int i{0}; i<n; i++)
-            t.push_back({nums[i]-k, nums[i]+k});
+        int i{0}, j{1};
 
-        sort(t.begin(), t.end());      
-        queue<int> q;
+        int ans{1};
 
-        int ans{0};
-
-        for(auto [x, y]:t)
+        while(j<n)
         {
-            while(!q.empty() and q.front()<x)
-                q.pop();
+            while(i<j and nums[j] - nums[i] > k*2)
+            {
+                i++;
+            }
 
-            q.push(y);
-
-            if(ans<q.size())
-                ans = q.size();
+            j++;
+            if(ans < j-i)
+            {
+                ans = j-i;
+            }
         }
 
         return ans;
