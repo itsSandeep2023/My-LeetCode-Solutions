07c7dce2cfeef9db747d3215a822c427be51af76
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-12 12:54:00 +0530

    Time: 2 ms (61.64%), Space: 13.1 MB (62.09%) - LeetHub

---
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int> pq;
+
+        long long ans{0};
+
+        for (auto& x : gifts) {
+            pq.push(x);
+        }
+
+        while (k--) {
+            pq.push(sqrt(pq.top()));
+            pq.pop();
+        }
+
+        while (!pq.empty()) 
+        {
+            ans += pq.top();
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
