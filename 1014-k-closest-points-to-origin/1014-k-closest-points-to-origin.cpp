1d8b261fc6e540820a681c5e1315d18ed9eb4cdb
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-12 13:55:19 +0530

    Time: 355 ms (10.36%), Space: 68.5 MB (63.7%) - LeetHub

---
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    #define P pair<float, int>
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<P, vector<P>, greater<P>> pq;
+
+        for(int i{0}; i<points.size(); i++)
+        {
+            int a = points[i][0]*points[i][0];
+            int b = points[i][1]*points[i][1];
+
+            float out = sqrt((float)(a+b));
+            cout<<out<<" ";
+
+            pq.push({out, i});
+        }
+
+        vector<vector<int>> ans;
+    
+        while(k-- and !pq.empty())
+        {
+            int c = pq.top().second;
+            pq.pop();
+            ans.push_back(points[c]);
+        }
+        
+        return ans;
+    }
+};
