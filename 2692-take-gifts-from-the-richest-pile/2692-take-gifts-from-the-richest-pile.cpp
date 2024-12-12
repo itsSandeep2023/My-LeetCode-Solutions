d046207cda597370e8d418fb1ac1ca3721e7cfd4
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-12 12:54:41 +0530

    Time: 0 ms (100%), Space: 13.4 MB (16.26%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
+        ios::sync_with_stdio(false);
+        
         priority_queue<int> pq;
 
         long long ans{0};
