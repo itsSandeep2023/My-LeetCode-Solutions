1528806d612350d17ade01d59c1ea0bfc31475b5
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-12 19:47:01 +0530

    Time: 0 ms (100%), Space: 25.9 MB (14.57%) - LeetHub

--- 3651-transformed-array/3651-transformed-array.cpp
+++ 3651-transformed-array/3651-transformed-array.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> constructTransformedArray(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        
         int n = nums.size();
         vector<int> ans(n);
         for(int i{0}; i<n; i++)
