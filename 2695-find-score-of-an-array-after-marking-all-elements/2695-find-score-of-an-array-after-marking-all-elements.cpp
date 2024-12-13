b3efa460c7317176ec9fd3210275e01a2de490ab
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-13 14:21:02 +0530

    Time: 87 ms (77.98%), Space: 96.2 MB (98.04%) - LeetHub

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long findScore(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> idx(n);
+        iota(idx.begin(), idx.end(), 0);
+
+        sort(idx.begin(), idx.end(), [&](int a, int b) {
+            if (nums[a] == nums[b])
+                return a < b;
+            return nums[a] < nums[b];
+        });
+
+        vector<bool> marked(n, false);
+
+        long long score{0};
+        for (auto& x : idx) {
+            if (!marked[x]) {
+                score += nums[x];
+                if (x > 0)
+                    marked[x - 1] = true;
+                if (x < n - 1)
+                    marked[x + 1] = true;
+            }
+        }
+
+        // for(auto& x: idx)
+        // {
+        //     cout<<nums[x]<<" "<<x<<endl;
+        // }
+
+        return score;
+    }
+};
