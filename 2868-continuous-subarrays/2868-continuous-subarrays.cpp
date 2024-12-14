c277c42c071a6c5e79315a40d61681e4cc4ca4d5
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-14 20:18:49 +0530

    Time: 153 ms (47.31%), Space: 129.5 MB (39.5%) - LeetHub

---
+++ 2868-continuous-subarrays/2868-continuous-subarrays.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    #define P pair<int, int>
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        priority_queue<P> mxq;
+        priority_queue<P, vector<P>, greater<P>> mnq;
+
+        long long ans{0};
+        int l{0};
+
+        for(int i{0}, n= nums.size(); i<n;i++)
+        {
+            mxq.push({nums[i], i});
+            mnq.push({nums[i], i});
+
+            while(mxq.top().first - mnq.top().first > 2)
+            {
+                if(mxq.top().second < mnq.top().second)
+                {
+                    l = mxq.top().second + 1;
+                    mxq.pop();
+                }
+                else
+                {
+                    l = mnq.top().second + 1;
+                    mnq.pop();
+                }
+
+                while(!mxq.empty() and mxq.top().second < l)
+                {
+                    mxq.pop();
+                }
+
+                while(!mnq.empty() and mnq.top().second < l)
+                {
+                    mnq.pop();
+                }
+            }
+            ans += i-l+1;
+        }
+
+        return ans;
+    }
+};
