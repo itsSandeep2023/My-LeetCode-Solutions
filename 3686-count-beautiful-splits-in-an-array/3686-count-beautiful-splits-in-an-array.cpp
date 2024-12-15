5315b533768017b6ef612c4ddce49d1dad0fd093
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-15 22:03:10 +0530

    Time: 210 ms (100%), Space: 123 MB (69.23%) - LeetHub

---
+++ 3686-count-beautiful-splits-in-an-array/3686-count-beautiful-splits-in-an-array.cpp
@@ -0,0 +1,33 @@
+int lcs[5010][5010];
+
+class Solution {
+public:
+    int beautifulSplits(vector<int>& nums) {
+       int n = nums.size();
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                lcs[i][j] = 0;
+            }
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= i; j--) {
+                if (nums[i] != nums[j]) {
+                    lcs[i][j] = 0;
+                } else {
+                    lcs[i][j] = 1 + lcs[i + 1][j + 1];
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 1; i <= n - 2; i++) {
+            for (int j = i + 1; j <= n - 1; j++) {
+                // [0, i - 1], [i, j - 1], [j, n - 1]
+                int len1 = i, len2 = j - i, len3 = n - j;
+                bool ok = len1 <= len2 && lcs[0][i] >= len1;
+                ok |= (len2 <= len3 && lcs[i][j] >= len2);
+                ans += int(ok);
+            }
+        }
+        return ans;
+    }
+};
