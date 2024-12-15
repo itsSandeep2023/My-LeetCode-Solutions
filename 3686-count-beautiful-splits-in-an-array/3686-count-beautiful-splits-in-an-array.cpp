d2f94ad63a4aa9e0749fe18f6978b6721b047dd2
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-15 22:15:25 +0530

    Time: 363 ms (92.31%), Space: 390 MB (23.08%) - LeetHub

--- 3686-count-beautiful-splits-in-an-array/3686-count-beautiful-splits-in-an-array.cpp
+++ 3686-count-beautiful-splits-in-an-array/3686-count-beautiful-splits-in-an-array.cpp
@@ -1,33 +1,25 @@
-
-int lcs[5001][5001];
 class Solution {
 public:
     int beautifulSplits(vector<int>& nums) {
-       int n = nums.size();
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= n; j++) {
-                lcs[i][j] = 0;
-            }
-        }
+        int n = nums.size();
+
+        vector<vector<int>> t(n+1, vector<int>(n+1));
+
         for (int i = n - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= i; j--) {
-                if (nums[i] != nums[j]) {
-                    lcs[i][j] = 0;
-                } else {
-                    lcs[i][j] = 1 + lcs[i + 1][j + 1];
-                }
+            for (int j = n - 1; j >= 0; j--) {
+                if (nums[i] == nums[j]) 
+                    t[i][j] = 1 + t[i + 1][j + 1];
             }
         }
-        int ans = 0;
-        for (int i = 1; i <= n - 2; i++) {
-            for (int j = i + 1; j <= n - 1; j++) {
-                // [0, i - 1], [i, j - 1], [j, n - 1]
-                int len1 = i, len2 = j - i, len3 = n - j;
-                bool ok = len1 <= len2 && lcs[0][i] >= len1;
-                ok |= (len2 <= len3 && lcs[i][j] >= len2);
-                ans += int(ok);
+
+        int ans{0};
+
+       for(int i=0;i<n-2;i++) {
+            for(int j=i+1;j<n-1;j++) {
+                if((t[0][i+1]>=i+1 && j-i>=i+1) || (t[i+1][j+1]>=j-i && n-j>=j-i)) ++ans;
             }
         }
+
         return ans;
     }
 };
