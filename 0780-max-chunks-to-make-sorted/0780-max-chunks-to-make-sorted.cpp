fd6fbbe3d969b8f2f871ac6c853b5b29ff16c64c
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-19 21:52:53 +0530

    Time: 0 ms (100%), Space: 9.3 MB (20.72%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -2,19 +2,10 @@ class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
         int ans{0};
-
-        for(int i{0}, n = arr.size(); i<n; i++)
-        {
-            if(arr[i] > i)
-            {
-                int mx = arr[i];
-                while(i<mx)
-                {
-                    i++;
-                    mx = max(mx, arr[i]);
-                }
-            }
-            ans++; 
+        int sum{0};
+        for (int i{0}, n = arr.size(); i < n; i++) {
+            sum+=arr[i] - i;
+            ans+=(sum==0);
         }
 
         return ans;
