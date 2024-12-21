49b5a3f9caa7f3ac4afe2ac2e50a36f102af9fe4
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-21 23:57:09 +0530

    Time: 110 ms (68.99%), Space: 176 MB (67.09%) - LeetHub

---
+++ 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        if (n < 2) return 1;
+
+        vector<vector<int>> gr(n);
+        vector<int> inDegree(n);
+        vector<long long> nodeValues(values.begin(), values.end());
+
+        for (const auto& edge : edges) {
+            int u = edge[0], v = edge[1];
+            gr[u].push_back(v);
+            gr[v].push_back(u);
+            inDegree[u]++;
+            inDegree[v]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 1) q.push(i);
+        }
+
+        int componentCount = 0;
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+            inDegree[cur]--;
+            long long addValue = (nodeValues[cur] % k == 0) ? 0 : nodeValues[cur];
+            if (addValue == 0) componentCount++;
+
+            for (int neighbor : gr[cur]) {
+                if (inDegree[neighbor] > 0) {
+                    inDegree[neighbor]--;
+                    nodeValues[neighbor] += addValue;
+                    if (inDegree[neighbor] == 1) q.push(neighbor);
+                }
+            }
+        }
+
+        return componentCount;
+    }
+};
