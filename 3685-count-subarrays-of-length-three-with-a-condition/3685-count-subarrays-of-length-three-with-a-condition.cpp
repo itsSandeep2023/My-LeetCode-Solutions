103b061741acba148b8d283274429db9f71e2a2c
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-21 22:23:52 +0530

    Time: 3 ms (22.22%), Space: 48.5 MB (55.56%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cpp
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     int countSubarrays(vector<int>& nums) {
         int ans{0};
-        int sum = nums[0] + nums[1] + nums[2];
-        if ((sum*2 == nums[1]*3))
-            ans++;
-        cout<<sum;
-        for (int i{3}, n = nums.size(); i < n; i++) {
-            sum += nums[i] - nums[i - 3];
-            if (sum*2 == (nums[i-1] * 3))
-                ans++;
+
+        for (int i{0}, n = nums.size(); i + 2 < n; i++) {
+            if ((nums[i] + nums[i + 2]) * 2 == nums[i + 1]) {
+                ans += 1;
+            }
         }
 
         return ans;
