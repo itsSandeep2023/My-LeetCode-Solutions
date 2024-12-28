34bdf284e2eab673b5512dc15b7c05b372ad00bd
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-28 20:36:50 +0530

    Time: 0 ms (100%), Space: 26.2 MB (65.58%) - LeetHub

--- 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
@@ -2,43 +2,51 @@ class Solution {
 public:
     vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> prefsum(n-k+1);
 
-        prefsum[0] = accumulate(nums.begin(), nums.begin()+k, 0);
+        // just k size sum arr and note there can be only n-k+1 arrs are possible 
+        vector<int> prefsum(n - k + 1);
 
-        for(int i{0}; i<n-k; i++)
-        {
-            prefsum[i+1] = prefsum[i] - nums[i] + nums[i+k];
+        // first k sum
+        prefsum[0] = accumulate(nums.begin(), nums.begin() + k, 0);
+
+        for (int i{0}; i < n - k; i++) {
+            prefsum[i + 1] = prefsum[i] - nums[i] + nums[i + k];
         }
 
-        vector<int> mx1(n-k+1);
+        // maxsum idx for left 1st sub arr
+        vector<int> mx1(n - k + 1);
+        // starts from idx 0
         mx1[0] = 0;
-        for(int i{1}; i<n-k; i++)
-        {
-            mx1[i] = prefsum[i] > prefsum[mx1[i-1]] ? i : mx1[i-1];
+        for (int i{1}; i < n - k; i++) {
+            // note > because need smallest idx for left to right traverse
+            mx1[i] = prefsum[i] > prefsum[mx1[i - 1]] ? i : mx1[i - 1];
         }
 
-        vector<int> mx3(n-k+1);
-        mx3[n-k] = n-k;
-        for(int i{n-k-1}; i>=0; i--)
-        {
-            mx3[i] = prefsum[i] >= prefsum[mx3[i+1]] ? i : mx3[i+1] ;
+        // maxsum idx for right 3rd sub arr
+        vector<int> mx3(n - k + 1);
+        // starts from idx n - k
+        mx3[n - k] = n - k;
+        for (int i{n - k - 1}; i >= 0; i--) {
+            // note >= because need smallest idx for right to left traverse
+            mx3[i] = prefsum[i] >= prefsum[mx3[i + 1]] ? i : mx3[i + 1];
         }
 
-        int a,b,c;
+        // need tree idx let a, b, c
+        int a, b, c;
         int maxsum{0};
-        for(int i{k}; i<=n-k*2; i++)
-        {
-            int csum = prefsum[mx1[i-k]] + prefsum[i] + prefsum[mx3[i+k]];
-            if(csum > maxsum)
-            {
+        for (int i{k}; i <= n - k * 2; i++) {
+            int csum = prefsum[mx1[i - k]] + prefsum[i] + prefsum[mx3[i + k]];
+            // store idxs only if csum > prev maxsum
+            if (csum > maxsum) {
+                // update values;
                 maxsum = csum;
-                a=mx1[i-k];
-                b=i;
-                c=mx3[i+k];
+                a = mx1[i - k];
+                b = i;
+                c = mx3[i + k];
             }
         }
 
+        // return as vector
         return {a, b, c};
     }
 };
