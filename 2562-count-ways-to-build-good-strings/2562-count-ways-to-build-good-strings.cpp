6d4988ece181000c6cf4a906e399f14e2f56dd0e
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-30 21:35:22 +0530

    Time: 6 ms (63.6%), Space: 14.4 MB (55.77%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -1,22 +1,24 @@
-const int mod=1e9+7;
+const int mod = 1e9 + 7;
 int dp[100001];
 class Solution {
 public:
-    static int good(int n, int x0, int x1){
-        if ( dp[n]!=-1 ) return dp[n];
+    static int good(int n, int x0, int x1) {
+        if (dp[n] != -1)
+            return dp[n];
         long long ans{0};
-        
-        if ( n >= x0)
-            ans+=1+good(n-x0, x0, x1);
-        
-        if ( n >= x1)
-            ans+=1+good(n-x1, x0, x1);
-        
-        return dp[n]=ans%mod;
+
+        if (n >= x0)
+            ans += 1 + good(n - x0, x0, x1);
+
+        if (n >= x1)
+            ans += 1 + good(n - x1, x0, x1);
+
+        return dp[n] = ans % mod;
     }
     static int countGoodStrings(int low, int high, int zero, int one) {
-        if (zero > one) swap(zero, one);
-        fill(dp, dp+high+1, -1);
-        return (good(high, zero, one)-good(low-1, zero, one)+mod)%mod;
+        if (zero > one)
+            swap(zero, one);
+        fill(dp, dp + high + 1, -1);
+        return (good(high, zero, one) - good(low - 1, zero, one) + mod) % mod;
     }
 };
