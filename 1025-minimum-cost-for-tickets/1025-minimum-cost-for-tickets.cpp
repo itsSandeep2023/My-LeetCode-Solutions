3e29bc6cca5b3d5e57d764c8a43b98e6750d9ebb
skid <136233661+itsSandeep2023@users.noreply.github.com>
2024-12-31 16:19:47 +0530

    Time: 1 ms (36.2%), Space: 13 MB (55.34%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int t[366+30] = {0};
+        int t[366 + 30] = {0};
 
-        unordered_set<int> st;
-
-        for(auto& d : days)
-            st.insert(d);
+        unordered_set<int> st(days.begin(), days.end());
 
         int n = days.back();
 
-        for(int i{30}; i<=n+29; i++)
-        {
-            if(!st.count(i-29))
+        for (int i{30}; i <= n + 29; i++) {
+            if (!st.count(i - 29))
                 t[i] = t[i - 1];
             else
-                t[i] = min({t[i - 1] + costs[0], t[i - 7] + costs[1], t[i - 30] + costs[2]});
-
-                cout<<t[i]<<" ";
+                t[i] = min({t[i - 1] + costs[0], t[i - 7] + costs[1],
+                            t[i - 30] + costs[2]});
         }
 
-        return t[n+29];
+        return t[n + 29];
     }
 };
