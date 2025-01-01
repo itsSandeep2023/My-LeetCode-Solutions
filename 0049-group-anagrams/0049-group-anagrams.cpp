7f4b593648d76b11847af8aea6572b0d21404cf4
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-01-01 12:49:10 +0530

    Time: 15 ms (76.92%), Space: 23.1 MB (95.96%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string, vector<string>> mp;
-        for (int i = 0; i < strs.size(); i++) {
-            string temp = strs[i];
-            sort(temp.begin(), temp.end());
-            mp[temp].push_back(strs[i]);
+        unordered_map<string, vector<string>> mp;
+
+        for(auto & str: strs)
+        {
+            auto x = str;
+            sort(x.begin(), x.end());
+            mp[x].push_back(str);
         }
-        vector<vector<string>> ans;
-        for (auto it : mp) {
-            ans.push_back(it.second);
+        vector<vector<string>> result(mp.size());
+
+        int index = 0;
+        for(auto & items: mp)
+        {
+            result[index++] = std::move(items.second);
         }
-        return ans;
+        return result;
     }
 };
