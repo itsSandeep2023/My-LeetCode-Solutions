1b616e8d709a45ddda1d191377b32274b9c590da
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-01-03 13:10:16 +0530

    Time: 3 ms (58.82%), Space: 97.2 MB (21.46%) - LeetHub

---
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long> prefsum(n);
+        prefsum[0] += nums[0];
+        for (int i{1}; i < n; i++)
+            prefsum[i] += prefsum[i - 1] + nums[i];
+
+        int ans{0};
+        for (int i{0}; i < n - 1; i++) {
+            if (prefsum[i] * 2 >= prefsum[n - 1])
+                ans++;
+        }
+
+        return ans;
+    }
+};
