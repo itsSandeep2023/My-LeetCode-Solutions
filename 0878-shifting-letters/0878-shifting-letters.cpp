ce0d85399b0563f21934cd82d76251773d93a708
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-01-05 15:19:34 +0530

    Time: 4 ms (63.88%), Space: 72.5 MB (38.13%) - LeetHub

---
+++ 0878-shifting-letters/0878-shifting-letters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n = s.size();
+        int sum{0};
+        string ans(n, ' ');
+
+        for (int i{n - 1}; i >= 0; i--) {
+            sum = (sum +shifts[i])%26;
+            int csf = (sum + s[i] - 'a') % 26;
+            ans[i] = csf + 'a';
+        }
+
+        return ans;
+    }
+};
