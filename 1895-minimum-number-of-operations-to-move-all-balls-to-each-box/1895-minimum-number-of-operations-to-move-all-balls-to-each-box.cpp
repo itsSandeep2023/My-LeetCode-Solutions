2caba6bbf9eca97c9205fed1ed790c2e39e317b4
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-01-06 17:05:17 +0530

    Time: 0 ms (100%), Space: 11.9 MB (76.52%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,29 +2,21 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n = boxes.size();
-        vector<int> l(n+1);
-        int csum{0};
-        for(int i{0}; i<n; i++)
-        {
-            csum += boxes[i]=='1'?1:0;
-            l[i+1] = l[i] + csum;
-        }
-
-        vector<int> r(n+1);
-        csum = 0;
-        for(int i{n-1}; i>=0; i--)
-        {
-            csum+=boxes[i]=='1'?1:0;
-            r[i] = r[i+1] + csum;
-        }
-
         vector<int> ans(n);
-
-        for(int i{0}; i<n; i++)
-        {
-            ans[i] = l[i] + r[i+1];
+        int l{0}, csum{0};
+        for (int i{0}; i < n; i++) {
+            ans[i]=l;
+            csum += boxes[i] == '1' ? 1 : 0;
+            l += csum;
         }
 
+        csum = 0;
+        int r{0};
+        for (int i{n - 1}; i >= 0; i--) {
+            ans[i]+=r;
+            csum += boxes[i] == '1' ? 1 : 0;
+            r += csum;
+        }
 
         return ans;
     }
