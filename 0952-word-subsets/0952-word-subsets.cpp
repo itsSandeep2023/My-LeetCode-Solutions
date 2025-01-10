9091f6b3dd3d16da83381af089af7b06c5876a03
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-01-10 19:37:38 +0530

    Time: 32 ms (72.15%), Space: 84.3 MB (72.15%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isSubset(vector<int>& freq2, vector<int>& temp) {
+        for (int i = 0; i < 26; i++) {
+            if (temp[i] < freq2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> result;
+
+        vector<int> freq2(26);
+        for (string& word : words2) {
+            int temp[26] = {0};
+
+            for (char& ch : word)
+                temp[ch - 'a']++;
+
+            for (int i{0}; i < 26; i++) {
+                freq2[i] = max(freq2[i], temp[i]);
+            }
+        }
+        for (string& word : words1) {
+            vector<int> temp(26, 0);
+
+            for (char& ch : word) {
+                temp[ch - 'a']++;
+            }
+
+            if (isSubset(freq2, temp) == true) {
+                result.push_back(word);
+            }
+        }
+
+        // T.C : O(n*l1 m*l2) = overall saare characters of words1 and words2
+        // S,C : O(26) ~= O(1)
+
+        return result;
+    }
+};
