f4a6ecb9dc8b5994cc92c69a5013309aaddda3ae
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-01-25 23:53:08 +0530

    Time: 1960 ms (5.4%), Space: 135.3 MB (91.89%) - LeetHub

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -4,39 +4,33 @@ public:
         int n = nums.size();
         vector<pair<int, int>> vec;
 
-        for(int i{0}; i<n; i++)
+        for (int i{0}; i < n; i++)
             vec.push_back({nums[i], i});
-        
+
         sort(vec.begin(), vec.end());
 
         vector<int> curr;
         int l{0};
         curr.push_back(vec[0].second);
 
-        for(int i{1}; i<n; i++)
-        {
-            if(vec[i].first - vec[i-1].first <= limit)
-            {
+        for (int i{1}; i < n; i++) {
+            if (vec[i].first - vec[i - 1].first <= limit) {
                 curr.push_back(vec[i].second);
-            }
-            else
-            {
+            } else {
                 sort(curr.begin(), curr.end());
-                for(auto& x : curr)
-                {
-                    cout<<x<<" "<<vec[l].first<< " -> ";
+                for (auto& x : curr) {
+                    cout << x << " " << vec[l].first << " -> ";
                     nums[x] = vec[l].first;
                     l++;
                 }
-                cout<<endl;
+                cout << endl;
                 curr.clear();
                 curr.push_back(vec[i].second);
             }
         }
 
         sort(curr.begin(), curr.end());
-        for(auto& x : curr)
-        {
+        for (auto& x : curr) {
             nums[x] = vec[l].first;
             l++;
         }
