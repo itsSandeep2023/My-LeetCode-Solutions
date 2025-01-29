45ed8f1a149747224cbb3f09ba2e1265a16dd8cc
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-01-29 23:37:42 +0530

    Time: 36 ms (24.67%), Space: 151.5 MB (15.1%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,25 +1,25 @@
 class NumMatrix {
     vector<vector<long long>> matSum;
+
 public:
     NumMatrix(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
 
-        matSum.resize(n+1, vector<long long>(m+1, 0));
+        matSum.resize(n + 1, vector<long long>(m + 1, 0));
 
-        for(int i{0}; i<n; i++)
-        {
+        for (int i{0}; i < n; i++) {
             long long sum{0};
-            for(int j{0}; j<m; j++)
-            {
+            for (int j{0}; j < m; j++) {
                 sum += matrix[i][j];
-                matSum[i+1][j+1] =  matSum[i][j+1] + sum;
+                matSum[i + 1][j + 1] = matSum[i][j + 1] + sum;
             }
         }
     }
-    
+
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return matSum[row2+1][col2+1] - matSum[row2+1][col1] - matSum[row1][col2+1] + matSum[row1][col1];
+        return matSum[row2 + 1][col2 + 1] - matSum[row2 + 1][col1] -
+               matSum[row1][col2 + 1] + matSum[row1][col1];
     }
 };
 
