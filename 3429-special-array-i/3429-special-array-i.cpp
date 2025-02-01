39cb7b7439a5f29b9e64860c25b0aceb3b53b4de
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-02-01 17:49:34 +0530

    Time: 0 ms (100%), Space: 27.6 MB (86.97%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        int n = nums.size();
-        if(n<=1) return true;
-
-        for(int i{1}; i<n; i++)
-        {
+        for(int i{1}, n = nums.size(); i<n; i++)
             if((nums[i]&1) == (nums[i-1]&1))
                 return false;
-        }
 
         return true;
     }
