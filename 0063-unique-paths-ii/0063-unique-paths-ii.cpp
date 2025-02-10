afedd8c1eabb6809040e67fd9e795216f469121d
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-02-10 22:06:29 +0530

    Time: 3 ms (12.48%), Space: 11.5 MB (85.67%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obs) {
-        int n = obs.size();
-        int m = obs[0].size();
+        int m = obs.size(), n = obs[0].size();
+        vector<int> dp(n, 0);
+        dp[0] = obs[0][0] == 0 ? 1 : 0;
 
-        if(obs[n-1][m-1] == 1) return 0;
-
-        vector<vector<long long>> t(n, vector<long long>(m));
-        t[n - 1][m - 1] = 1;
-
-        for (int i{n - 2}; i >= 0; i--) {
-            if (obs[i][m - 1] == 1)
-                break;
-            t[i][m - 1] = 1;
-        }
-
-        for (int i{m - 2}; i >= 0; i--) {
-            if (obs[n - 1][i] == 1)
-                break;
-            t[n - 1][i] = 1;
-        }
-
-        for (int i{n - 2}; i >= 0; i--) {
-            for (int j{m - 2}; j >= 0; j--) {
-                if (obs[i][j] == 1)
-                    continue;
-
-                t[i][j] = t[i + 1][j] + t[i][j + 1];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (obs[i][j] == 1) {
+                    dp[j] = 0;
+                } else if (j > 0) {
+                    dp[j] += dp[j - 1];
+                }
             }
         }
 
-        return t[0][0];
+        return dp[n - 1];
     }
 };
