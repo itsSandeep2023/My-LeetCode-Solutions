9aac3f181d51b41ad9bd92915235b54cb37e520c
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-02-17 14:24:56 +0530

    Time: 28 ms (46.19%), Space: 16.8 MB (46.94%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -1,40 +1,30 @@
 class Solution {
     unordered_set<string> res;
     int n;
+
 public:
-    void solve(string &tiles, vector<bool> &ids, string& cur, int idx)
+    void solve(string& tiles, vector<bool>& ids, string& cur)
     {
-        if(idx>=n)
-        {
-            if(!res.count(cur))
-            {
-                res.insert(cur);
-                cout<<cur<<" ";
-
-            }
-            return;
-        }
-
-        for(int i{0}; i<n; i++)
-        {
-            if(ids[i]==false)
-            {
+        for (int i{0}; i < n; i++) {
+            if (ids[i] == false) {
                 cur.push_back(tiles[i]);
-                ids[i]=true;
-                solve(tiles, ids, cur, idx+1);
+                if(!res.count(cur))
+                {
+                    res.insert(cur);
+                    ids[i] = true;
+                    solve(tiles, ids, cur);
+                    ids[i] = false;
+                }
                 cur.pop_back();
-                ids[i]=false;
             }
         }
-
-        solve(tiles, ids, cur, idx+1);
     }
 
     int numTilePossibilities(string tiles) {
         n = tiles.size();
         vector<bool> ids(n, false);
         string cur;
-        solve(tiles, ids, cur, 0);
-        return res.size()-1;
+        solve(tiles, ids, cur);
+        return res.size();
     }
 };
