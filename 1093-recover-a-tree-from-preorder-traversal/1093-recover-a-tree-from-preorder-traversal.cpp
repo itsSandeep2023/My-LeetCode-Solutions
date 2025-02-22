8585278934c3ffccd1e5208565c638bc63d466d0
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-02-22 15:48:13 +0530

    Time: 0 ms (100%), Space: 22.5 MB (27.13%) - LeetHub

--- 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.cpp
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.cpp
@@ -6,42 +6,38 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
     int n;
+
 public:
-    TreeNode* solve(string &tr, int &idx, int &level, int &nlevel)
-    {
-        if(idx>=n)
+    TreeNode* solve(string& tr, int& idx, int& level, int& nlevel) {
+        if (idx >= n)
             return NULL;
 
         int cnt{0};
-        while(idx<n and tr[idx] != '-')
-        {
+        while (idx < n and tr[idx] != '-') {
             cnt++;
             idx++;
         }
 
-        TreeNode* root = new TreeNode(stoi(tr.substr(idx-cnt, cnt)));
-        cout<< level << " -> "<<root->val<<endl;
-        nlevel=0;
-        while(idx<n and tr[idx]=='-')
-        {
+        TreeNode* root = new TreeNode(stoi(tr.substr(idx - cnt, cnt)));
+        nlevel = 0;
+        while (idx < n and tr[idx] == '-') {
             idx++;
             nlevel++;
         }
 
-        if(level < nlevel)
-        {
+        if (level < nlevel) {
             level++;
             root->left = solve(tr, idx, level, nlevel);
             level--;
         }
 
-        if(level < nlevel)
-        {
+        if (level < nlevel) {
             level++;
             root->right = solve(tr, idx, level, nlevel);
             level--;
