562d858c2b3082093c87cfd3c890427fa028a95d
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-02-22 15:45:33 +0530

    Time: 51 ms (8.75%), Space: 22.7 MB (19.47%) - LeetHub

---
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int n;
+public:
+    TreeNode* solve(string &tr, int &idx, int &level, int &nlevel)
+    {
+        if(idx>=n)
+            return NULL;
+
+        int cnt{0};
+        while(idx<n and tr[idx] != '-')
+        {
+            cnt++;
+            idx++;
+        }
+
+        TreeNode* root = new TreeNode(stoi(tr.substr(idx-cnt, cnt)));
+        cout<< level << " -> "<<root->val<<endl;
+        nlevel=0;
+        while(idx<n and tr[idx]=='-')
+        {
+            idx++;
+            nlevel++;
+        }
+
+        if(level < nlevel)
+        {
+            level++;
+            root->left = solve(tr, idx, level, nlevel);
+            level--;
+        }
+
+        if(level < nlevel)
+        {
+            level++;
+            root->right = solve(tr, idx, level, nlevel);
+            level--;
+        }
+
+        return root;
+    }
+
+    TreeNode* recoverFromPreorder(string tr) {
+        this->n = tr.size();
+        int idx{0}, level{0}, nlevel{0};
+        return solve(tr, idx, level, nlevel);
+    }
+};
