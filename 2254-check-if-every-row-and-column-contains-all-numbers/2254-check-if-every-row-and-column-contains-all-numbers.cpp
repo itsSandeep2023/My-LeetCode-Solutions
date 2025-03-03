12a2611449cc79c85c1fe38315387e6819c265d6
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-03-03 16:22:08 +0530

    Time: 4 ms (86.56%), Space: 39.1 MB (83.59%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -3,19 +3,15 @@ public:
     bool checkValid(vector<vector<int>>& matrix) {
         int n = matrix.size();
         vector<int> ans(n + 1);
+        vector<int> ans1(n + 1);
 
         for (int i{0}; i < n; i++) {
             for (int j{0}; j < n; j++) {
                 ans[matrix[i][j]]++;
                 if (ans[matrix[i][j]] > i + 1)
                     return false;
-            }
-        }
-
-        for (int j{0}; j < n; j++) {
-            for (int i{0}; i < n; i++) {
-                ans[matrix[i][j]]++;
-                if (ans[matrix[i][j]] > n + j + 1)
+                ans1[matrix[j][i]]++;
+                if (ans1[matrix[j][i]] > i + 1)
                     return false;
             }
         }
