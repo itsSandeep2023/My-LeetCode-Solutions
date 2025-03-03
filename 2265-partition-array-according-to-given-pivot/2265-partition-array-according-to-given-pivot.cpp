a2e9a4f7dfbb0916a0e39a8e21dac053aedb4107
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-03-03 15:38:45 +0530

    Time: 6 ms (64.75%), Space: 127.7 MB (87.5%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
-        vector<int> ans;
-        vector<int> ans1;
+        vector<int> ans(n);
+        int i{0};
         int ps{0};
 
-        for(auto& x : nums)
-        {
-            if(x==pivot) ps++;
-            else if(x<pivot) ans.push_back(x);
-            else ans1.push_back(x);
+        for (auto& x : nums) {
+            if (x == pivot)
+                ps++;
+            else if (x < pivot)
+                ans[i++] = x;
         }
 
-        while(ps--)
-            ans.push_back(pivot);
-        
-        for(auto& x : ans1)
-            ans.push_back(x);
+        while (ps--)
+            ans[i++] = pivot;
+
+        for (auto& x : nums)
+            if (x > pivot)
+                ans[i++] = x;
 
         return ans;
     }
