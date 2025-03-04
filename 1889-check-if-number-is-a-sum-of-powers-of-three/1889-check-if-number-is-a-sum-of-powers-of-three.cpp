33b5df71b04ff8e0b26e87e108a92b2c67af29f0
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-03-04 11:03:28 +0530

    Time: 0 ms (100%), Space: 8.3 MB (9.62%) - LeetHub

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    int log3(int a) { return log2(a) / log2(3); }
+
+public:
+    bool checkPowersOfThree(int n) {
+        int t;
+        unordered_set<int> st;
+        while(n)
+        {
+            t = log3(n);
+            if(st.count(t)) return false;
+            st.insert(t);
+            n -= pow(3, t);
+        }
+
+        return true;
+    }
+};
