a50e7c4265ee183efec925466abc1503893d365f
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-03-05 11:54:21 +0530

    Time: 0 ms (100%), Space: 8 MB (31.17%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        n--;
-        return 1 + ((8 + (n-1)*4)*1ll*(n))/2;
+        return 1 + ((8 + (--n-1)*4)*1ll*(n))/2;
     }
 };
