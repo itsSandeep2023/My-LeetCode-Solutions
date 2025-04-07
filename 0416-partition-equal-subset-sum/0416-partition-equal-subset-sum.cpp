200d60155f5e7634c703985a20cff6b27bd03e25
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-04-07 12:01:06 +0530

    Time: 2 ms (99.68%), Space: 14.2 MB (84.78%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int totalSum = 0;
-        for (int num : nums)
-            totalSum += num;
-
-        if (totalSum & 1)
-            return false;
-
-        int targetSum = totalSum / 2;
-        vector<bool> dp(targetSum + 1, false);
-        dp[0] = true;
-        for (int num : nums) {
-            for (int currSum = targetSum; currSum >= num; --currSum) {
-                dp[currSum] = dp[currSum] || dp[currSum - num];
-                if (dp[targetSum])
-                    return true;
-            }
+        int s = accumulate(nums.begin(), nums.end(), 0);
+        if(s%2) return false;
+        bitset<10001> bit(1);
+        for(auto i : nums){
+            bit |= bit<<i;
+            if(bit[s/2]) return true;
         }
-        return dp[targetSum];
+        return bit[s/2];
     }
 };
