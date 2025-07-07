2cb6e3a748c4602de4cfdc6d822d6ad9ddfbbffe
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-07-07 12:44:44 +0530

    Time: 66 ms (41.24%), Space: 74.8 MB (85.96%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -10,18 +10,6 @@ public:
         priority_queue<int, vector<int>, greater<int>> pq;
 
         while (i < n or !pq.empty()) {
-            if (i < n) {
-                while (day < events[i][0] and !pq.empty()) {
-                    if (pq.top() >= day) {
-                        ans++;
-                        day++;
-                    }
-                    pq.pop();
-                }
-
-                day = events[i][0];
-            }
-
             while (i < n and events[i][0] == day) {
                 pq.push(events[i][1]);
                 i++;
