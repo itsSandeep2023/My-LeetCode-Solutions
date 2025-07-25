764d8d252232a565311bf1c09b7c8031537ac761
skid <136233661+itsSandeep2023@users.noreply.github.com>
2025-07-25 16:04:33 +0530

    Time: 1 ms (37.33%), Space: 29.4 MB (75.17%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int maxSum(vector<int>& nums) {
         unordered_set<int> st;
-        
+
         int ans = *max_element(nums.begin(), nums.end());
 
-        if(ans<=0) return ans;
+        if (ans <= 0)
+            return ans;
 
         ans = 0;
 
-        for(int &x : nums)
-        {
-            if(x>0 && !st.count(x))
-            {
-                ans+=x;
+        for (int& x : nums) {
+            if (x > 0 && !st.count(x)) {
+                ans += x;
                 st.insert(x);
             }
         }
